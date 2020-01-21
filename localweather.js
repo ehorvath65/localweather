bb825748f0c8648d42cd3c21585aa50b50ae78a0
Edit Horváth <edit.horvath@gmail.com>
2020-01-21 21:55:12 +0100

    Changed location api

--- localweather.js
+++ localweather.js
@@ -1,39 +1,39 @@
-
-
-var google_api = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
 var darksky_api = "https://api.darksky.net/forecast/4e6cca0fe2052f9f2b8c230f148635a8/";
 var latitude = 0;
 var longitude = 0;
 var currently = "";
 var cels = true;
 
+navigator.geolocation.getCurrentPosition(success, error, options);
 var options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
 };
-
 function success(myPlace) {
     var crd = myPlace.coords;
     longitude = crd.longitude;
     latitude = crd.latitude;
-    //console.log(latitude);
-    //console.log(longitude);
     document.getElementById("coords").innerHTML = "latitude: " + latitude.toFixed(2) + " longitude: " + longitude.toFixed(2);
-    getLocation();
+    locations();
     getWeather();
 }
-
 function error(err) {
     //console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-navigator.geolocation.getCurrentPosition(success, error, options);
-
-function getLocation() {
-    $.getJSON(google_api + latitude + ", " + longitude + "&language=hu",
+function locations() {
+    var apiKey = "&key=929ff7ade8cc41e99b688b0ba3740134";
+    $.getJSON(
+        "https://api.opencagedata.com/geocode/v1/json?q=" + latitude + "+" + longitude + apiKey,
         function (data) {
-            $("#location").html(data.results[2].formatted_address);
+            var components = data.results[0].components;
+            var city = components.city;
+            var postcode = components.postcode;
+            var city_district = components.city_district;
+            var suburb = components.suburb;
+            var road = components.road;
+            $("#location").html(postcode + " " + city + ", " + suburb);
         }
     );
 }
@@ -51,46 +51,40 @@ function getShow() {
     $("#weather").html(currently.summary);
     console.log(currently);
 
-  $("#temperature").html(
-        Math.round((currently.temperature - 32) * 5 / 9) + " °C"
-    );
-
-  function celsius() {
     $("#temperature").html(
         Math.round((currently.temperature - 32) * 5 / 9) + " °C"
     );
-   cels=true;
-  }
-   function fahrenheit() {
-     $("#temperature").html(
-        Math.round(currently.temperature) + " °F"
-    );
-     cels=false;
-     }
-  $("#temperature").click(function(){
-   if (cels) {
-     fahrenheit();
-    }else{
-      celsius();
+
+    function celsius() {
+        $("#temperature").html(
+            Math.round((currently.temperature - 32) * 5 / 9) + " °C"
+        );
+        cels = true;
     }
+    function fahrenheit() {
+        $("#temperature").html(
+            Math.round(currently.temperature) + " °F"
+        );
+        cels = false;
+    }
+    $("#temperature").click(function () {
+        if (cels) {
+            fahrenheit();
+        } else {
+            celsius();
+        }
     });
 
-
-   $("#uvIndex").html(
-   "UV index: " + (currently.uvIndex)
+    $("#uvIndex").html(
+        "UV index: " + (currently.uvIndex)
     );
 
-   $("#windSpeed").html(
-   "Wind speed: " + (currently.windSpeed) + " m/s"
+    $("#windSpeed").html(
+        "Wind speed: " + (currently.windSpeed) + " m/s"
     );
-
 
     icon(currently);
-
-
-
 }
-
 
 function icon() {
     var skycons = new Skycons({
